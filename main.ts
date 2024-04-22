2165ba19e4aa5b89b8f43d622d3d53a7c00a6274
Ehsankhaan01 <you@example.com>
2024-04-23 00:57:02 +0500

    push my calculator

---
+++ main.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer'
+
+const cal2 = await inquirer.prompt([
+    {
+        type : "number" ,
+        name : "firstnumber",
+        message : "enter your first number",
+    },
+    {
+        type : "number",
+        name : "secondnumber",
+        message : "enter your second number",
+    
+    },
+    {
+        type : 'list',
+        name : "operator",
+        message : "plrase select an operator to perform action",
+        choices: ["addition", "subtraction","multiplication", "division",]
+    }
+]);
+
+if (cal2.operator === "addition"){
+    console.log(`the Sum of your number is ${cal2.firstnumber + cal2.secondnumber}`)
+}
+else if (cal2.operator === "subtraction"){
+    console.log(`the Sum of your number is ${cal2.firstnumber - cal2.secondnumber}`)
+} else if (cal2.operator === "multiplication"){
+    console.log(`the Sum of your number is ${cal2.firstnumber * cal2.secondnumber}`)
+}else if (cal2.operator === "divison"){
+    console.log(`the Sum of your number is ${cal2.firstnumber / cal2.secondnumber}`)
+};
+
+console.log("this is Ahsan khan best calculator"); 
